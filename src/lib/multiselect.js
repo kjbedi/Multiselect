59572085737bd8822aa98174195f725d484c14e4
kaushiksamanta <kaushiksamanta23@gmail.com>
2019-05-11 12:45:02 -0400

    Added multiselect elements

--- src/lib/multiselect.js
+++ src/lib/multiselect.js
@@ -12,7 +12,7 @@ export class Multiselect {
     }
 
     renderList() {
-        let liElements = this.contents.map(content => this.getLi(content.text));
+        let liElements = this.contents.map(content => this.getLi(content.text, content.isSelected));
         for( let li of liElements ){
             this.appendLi( li );
         }
@@ -22,9 +22,24 @@ export class Multiselect {
         this.ul.appendChild(li);
     }
 
-    getLi(text) {
-        let li = document.createElement( 'li' );
-        li.innerText = text;
+    getLi(text, isSelected) {
+        let li = document.createElement('li');
+        li.appendChild(this.getCheckBox(text, isSelected));
+        li.appendChild(this.getContainerText(text));
         return li;
+    }
+
+    getCheckBox(text, isSelected) {
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.value = text;
+        checkbox.checked = isSelected;
+        return checkbox
+    }
+
+    getContainerText(text) {
+        let containerText = document.createElement('span');
+        containerText.innerText = text;
+        return containerText
     }
 }
