5f2f1b4df69c9df3ab3171ea9754e08e220e7f23
kaushiksamanta <kaushiksamanta23@gmail.com>
2019-05-11 13:26:18 -0400

    Added input sanitizer to validate input

--- src/lib/multiselect.js
+++ src/lib/multiselect.js
@@ -1,7 +1,11 @@
+import { sanitizeInput } from '../util/sanitizer';
+
 export class Multiselect {
-    constructor(elem, contents) {
+    /// Param 1:- Takes the dom element within which the plugin will render elements.
+    /// Param 2:- Takes the contents in a array of object format..
+    constructor(elem, contents = []) {
         this.element = elem;
-        this.contents = contents;
+        this.contents, this.outputNodes = sanitizeInput(contents);
         this.init();
     }
 
@@ -12,7 +16,7 @@ export class Multiselect {
     }
 
     renderList() {
-        let liElements = this.contents.map(content => this.getLi(content.text, content.isSelected));
+        let liElements = this.contents.map((content, index) => this.getLi(content.text, content.isSelected, index));
         for( let li of liElements ){
             this.appendLi( li );
         }
@@ -22,24 +26,39 @@ export class Multiselect {
         this.ul.appendChild(li);
     }
 
-    getLi(text, isSelected) {
+    getLi(text, isSelected, index) {
         let li = document.createElement('li');
-        li.appendChild(this.getCheckBox(text, isSelected));
+        li.appendChild(this.getCheckBox(text, isSelected, index));
         li.appendChild(this.getContainerText(text));
         return li;
     }
 
-    getCheckBox(text, isSelected) {
+    getCheckBox(text, isSelected, index) {
         let checkbox = document.createElement("input");
         checkbox.type = "checkbox";
         checkbox.value = text;
         checkbox.checked = isSelected;
-        return checkbox
+        checkbox.addEventListener('change', () => {
+            this.outputNodes[index].isSelected = checkbox.checked;
+            this.parseOutput();
+         });
+        return checkbox;
     }
 
     getContainerText(text) {
         let containerText = document.createElement('span');
         containerText.innerText = text;
-        return containerText
+        return containerText;
+    }
+
+    parseOutput() {
+        const output = this.outputNodes.filter((content) => content.isSelected === true).map((content) => content.text)
+        if(this.changeFunc) {
+            this.changeFunc(output);
+        }
+    }
+
+    onChange(func) {
+        this.changeFunc = func;
     }
 }
